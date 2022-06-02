57288fbf566989ea727f217399df46c6fac3803a
Mario Kaack <mario@antsle.com>
2022-06-01 21:21:47 -0700

    Implement new data structure for iface info
    Collect all data from 'ip addr show'
    Function to filter data

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template
-from net_info import get_physical_interfaces, get_bridge_interfaces, get_interface_mac, get_iface_data
+from net_info import get_physical_iface_data, get_bridge_iface_data
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    physical_interfaces = get_physical_interfaces()
-    bridge_interfaces = get_bridge_interfaces()
-    iface_data = get_iface_data()
+def index2():
+    physical_iface_data = get_physical_iface_data()
+    bridge_iface_data = get_bridge_iface_data()
+    return render_template('index.html', physical_iface_data=physical_iface_data, bridge_iface_data=bridge_iface_data)
     
-    return render_template('index.html', physical_interfaces=physical_interfaces, bridge_interfaces=bridge_interfaces, iface_data=iface_data)
--- net_info.py
+++ net_info.py
@@ -1,4 +1,7 @@
 import subprocess
+import re
+import json
+import os
 from pprint import pprint
 
 
@@ -47,6 +50,7 @@ def is_physical_interface_connected(iface):
         f"cat /sys/class/net/{iface}/carrier",
         shell=True,
         stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
         universal_newlines=True,
         check=False
     ).stdout.strip()
@@ -57,31 +61,118 @@ def is_physical_interface_connected(iface):
     return False
 
 
-def get_iface_data():
-    iface_data = {}
-    for iface in get_physical_interfaces():
-        mac = get_interface_mac(iface)
-        connected = is_physical_interface_connected(iface)
-        #speed = get_interface_speed(iface)
-        iface_data.update({iface : { 'mac': mac, 'connected': connected }})
-    for iface in get_bridge_interfaces():
-        mac = get_interface_mac(iface)
-        using_physical_iface = 'n/a'
-        for phys_iface in get_physical_interfaces():
-            if mac == iface_data[phys_iface]['mac']:
-                using_physical_iface = phys_iface
-                break
-        iface_data.update({iface : { 'mac': mac, 'using': using_physical_iface }})
-                
-    return iface_data
+def ip_addr_show():
+    ip_addr_show = subprocess.run(
+        ["ip", "address", "show"],
+        stdout=subprocess.PIPE,
+        universal_newlines=True
+    )
+    return ip_addr_show.stdout
 
+
+def iface_speed(iface_name):
+    output = subprocess.run(
+        ["ethtool", iface_name],
+        universal_newlines=True,
+        stdout=subprocess.PIPE
+    ).stdout.split('\n')
+    speed_re = re.compile(r'(\d{2}\w+\/(Full|Half))')
+    speeds = set()
+    for line in output:
+        speed_matches = speed_re.findall(line)
+        if speed_matches:
+            for item in speed_matches:
+                speeds.add(item[0])
+        else:
+            continue
+    if not speeds:
+        return ''
+    return(sorted(speeds)[0])
+
+
+def create_iface_data_array(ip_addr_show_text):
+    '''
+    Returns an array of dicts:
+    [{'ifIndex': '2',
+      'ifName': 'eno1',
+      'ifStatus': 'UP',
+      'ifType': 'ether',
+      'ipv4': '',
+      'ipv6': '',
+      'mac': 'ac:1f:6b:48:4b:84',
+      'speed': '1000baseT/Full'},
+      {'ifIndex': '7',
+      'ifName': 'br0',
+      'ifStatus': 'UNKNOWN',
+      'ifType': 'ether',
+      'ipv4': '192.168.1.14',
+      'ipv6': 'fe80::ae1f:6bff:fe48:4b84/64',
+      'mac': 'ac:1f:6b:48:4b:84',
+      'speed': ''}
+      ... 
+    '''
+    data = []
+
+    iface_name_re = re.compile(r'^(\d{1,2}): ([^:]+):[^\n]+state (\w+)')
+    link_re = re.compile(r'^\s+link\/(ether) ([0-9a-fA-F:]+)')
+    ipv4_re = re.compile(r'^\s+inet (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\/(\d{1,2})')
+    ipv6_re = re.compile(r'^\s+inet6 (([0-9a-fA-F:]+)\/(\d{1,3}))')
+
+    for line in ip_addr_show_text.split('\n'):
+        iface_info = {'ifName': '', 'mac': '', 'ipv4': '', 'ipv6': ''}
+        match_name = iface_name_re.search(line)
+        if match_name:
+            iface_info['ifIndex'] = match_name.group(1)
+            iface_info['ifName'] = match_name.group(2)
+            iface_info['ifStatus'] = match_name.group(3)
+            iface_info['speed'] = iface_speed(match_name.group(2))
+            data.append(iface_info)
+            
+        match_mac = link_re.search(line)
+        if match_mac:
+            data[-1]['ifType'] = match_mac.group(1)
+            data[-1]['mac'] = match_mac.group(2)
+            
+        match_ipv4 = ipv4_re.search(line)
+        if match_ipv4:
+            data[-1]['ipv4'] = match_ipv4.group(1) 
+
+        match_ipv6 = ipv6_re.search(line)
+        if match_ipv6:
+            data[-1]['ipv6'] = match_ipv6.group(1)
+            
+    return data                
+
+def get_iface_data():
+    return create_iface_data_array(ip_addr_show())
+
+def filter_interface_data(iface_data, filter_list):
+    filtered_data = [] 
+    for iface in iface_data:
+        if iface['ifName'] in filter_list:
+            filtered_data.append(iface)
+    return filtered_data
+
+def get_physical_iface_data():
+    return filter_interface_data(get_iface_data(), get_physical_interfaces())
+
+def get_bridge_iface_data():
+    return filter_interface_data(get_iface_data(), get_bridge_interfaces())
 
 
 
 if __name__ == '__main__':
     print(get_physical_interfaces())
     print(get_bridge_interfaces())
-    pprint(get_iface_data())
     print(is_physical_interface_connected('eno1'))
     print(is_physical_interface_connected('eno2'))
+    print(is_physical_interface_connected('eno3'))
+    print(is_physical_interface_connected('eno4'))
     #print(get_interface_speed('br1'))
+    print(iface_speed('eno1'))
+    print(iface_speed('eno2'))
+    print(iface_speed('eno3'))
+    print(iface_speed('eno4'))
+    #pprint(create_iface_data_array(ip_addr_show()))
+    pprint(filter_interface_data(get_iface_data(), get_physical_interfaces()))
+    pprint(get_iface_data())
